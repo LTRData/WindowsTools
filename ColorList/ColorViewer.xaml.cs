c347a0d13bcf0be963fb46e0864e042bb7b78669
LTRData <ol@ltr-data.se>
2026-04-10 11:53:17 +0200

    Added ColorList tool

---
+++ ColorList/ColorViewer.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ColorList;
+
+public partial class ColorViewer : Window
+{
+    public ColorViewer()
+    {
+        InitializeComponent();
+
+        var brushes = typeof(SystemColors)
+            .GetProperties(BindingFlags.Static | BindingFlags.Public)
+            .Where(p => p.PropertyType == typeof(SolidColorBrush))
+            .Select(p => new
+            {
+                p.Name,
+                Brush = (SolidColorBrush?)p.GetValue(null, null)
+            })
+            .OrderBy(x => x.Name)
+            .ToList();
+
+        ColorList.ItemsSource = brushes;
+    }
+}
